e631b3527a74c89896c79bf73dc14d3b8972faa5
Вадим Зенков <vadim.zenkov@ingate>
2020-03-18 17:06:36 +0300

    create test rout

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-16 20:07:25.
+    * Generated on 2020-03-18 11:30:48.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
